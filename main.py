d50eb76d1f0c3c35d3cc4e8a28a30b6aa0548676
Mushrrom <mail@h1i.dev>
2023-03-27 21:08:44 +1100

    a

--- main.py
+++ main.py
@@ -1,43 +1,45 @@
+import interactions
 from dotenv import load_dotenv
-import discord
-from discord import Client, Intents, Embed
-import os
 import openai as openaii
-from discord.ext import commands
-from discord_slash import SlashCommand, SlashContext
+import os
 import random
-#Do I really need this many libraries?.. Yes ofc
+import json
 
-#Dumb boring setup stuff
 load_dotenv()
 openaii.api_key = os.getenv("OPENAI-TOKEN")
+bot = interactions.Client(
+    token=os.getenv("BOT-TOKEN")
+)
 
-bot = commands.Bot(command_prefix='-')
-slash = SlashCommand(bot, sync_commands=True)
 
-
-#Actual code fun stuff
-@bot.event
-async def on_ready():
-    print('If you can see this then something is working. Logged in as {0.user}'.format(bot))
-
-@slash.slash(name="ping", description="ping") #Every bot needs to keep the ping command
+@bot.command(name="ping", description="ping") #Every bot needs to keep the ping command
 async def test(ctx):
     await ctx.send("Pong!")
     print(ctx)
 
-@slash.slash(name="openai", description="Ask GPT-3 something")
-async def openai(ctx, message):
+
+@bot.command(name="openai",
+    description="say openai!",
+    options = [
+        interactions.Option(
+            name="message",
+            description="What you want to say",
+            type=interactions.OptionType.STRING,
+            required=True,
+        ),
+    ],)
+async def openai(ctx: interactions.CommandContext, message: str):
+    print(message)
     if random.randint(1, 2) == 1:
         selfpromo = "This bot was made by hii#6002"
     else:
         selfpromo = "The source of this project is available on github, check it out at https://github.com/Mushrrom/Openai-discord-bot"
     #embed creation
-    responseembed = discord.Embed(title="OpenAI text response", description = "Prompt: %s"%message, colour=discord.Colour.green())
+    responseembed = interactions.Embed(title="OpenAI text response", description = "Prompt: %s"%message, colour="#00ff00")
     responseembed.set_footer(text = selfpromo)
     responseembed.add_field(name = "Response", value = "Loading response...")
     responseembed.set_thumbnail(url="https://cdn.discordapp.com/attachments/974174975793709056/974187217658454036/lp-logo-3-888629431.png")
-    messagee = await ctx.send(embed=responseembed)
+    messagee = await ctx.send(embeds=[responseembed])
     #Wow thats annoying
     #Get response
     response = openaii.Completion.create(engine="text-babbage-001", prompt=message, temperature=1, max_tokens=400)
@@ -46,8 +48,8 @@ async def openai(ctx, message):
 
     
     responseembed.set_field_at(index=0, name="Response", value = "%s `%s`"%(message,response_txt))#set field
-    await messagee.edit(embed=responseembed)#Edit original message
+    await messagee.edit(embeds=responseembed)#Edit original message
     # what am i doing I HAVE ENGLISH HOMEWORK I NEED TO DO
     
     # What is it with me doing this project when I have english assignments
-bot.run(os.getenv("BOT-TOKEN"))
+bot.start()
